2e1494d277a57e2d4805e310b0af63633d38d7d0
RICK <dassouraasish+server@gmail.com>
2026-09-20 10:12:35 +0530

    Add Fly Tree-sitter grammar: queries/highlights.scm

---
+++ queries/highlights.scm
@@ -0,0 +1,38 @@
+; Keywords
+[
+  "group" "from" "job"
+  "if" "orif" "else" "ifnot"
+  "while" "for" "in"
+  "wait" "do" "grab"
+  "give" "skip" "getout"
+  "bring" "as"
+  "and" "or" "not"
+] @keyword
+
+; Built-in literals
+(boolean) @constant.builtin
+(emp) @constant.builtin
+(self) @variable.builtin
+(number) @number
+(string) @string
+
+; Declarations
+(group_declaration name: (identifier) @type)
+(job_declaration name: (identifier) @function)
+(field_declaration name: (identifier) @property)
+
+; Access
+(member_expression property: (identifier) @property)
+
+; Comments
+(line_comment) @comment
+(block_comment) @comment
+
+; Operators
+[
+  "=" "==" "!=" "<" "<=" ">" ">="
+  "+" "-" "*" "/" "%" ":"
+] @operator
+
+; Functions and calls
+(call_expression function: (identifier) @function)
